3adf9e62babed821e64540585d2fd3eda519c9ca
Ubuntu <ubuntu@ip-172-31-44-218.ap-southeast-2.compute.internal>
2024-03-06 00:11:21 +0000

    Add GnuPG

--- password_manager.sh
+++ password_manager.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+count=0
 while true
  do
+ if [ $count -eq 0 ]; then
  echo "パスワードマネージャーへようこそ！"
+ echo "あなたのGPGキーで設定したユーザー名を入力してください。"
+ read gpg_name
+ gpg_name="${gpg_name:?GPGのユーザー名を設定してください。}"
+ fi
+ ((count++))
  read -p $'次の選択肢から入力してください(Add Password/Get Password/Exit)：' Select
 
  case "$Select" in
@@ -9,13 +16,17 @@ while true
   read -p "サービス名を入力してください：" ServiceName
   read -p "ユーザー名を入力してください：" UserName
   read -p "パスワードを入力してください：" Password
+  gpg -d apprentice.asc > apprentice 2> /dev/null
   printf "%s,%s,%s\n" "$ServiceName" "$UserName" "$Password" >> ./apprentice
+  gpg -r "$gpg_name" -ea apprentice
   echo "パスワードの追加は成功しました。"
   #\n次の選択肢から入力してください(Add Password/Get Password/Exit)：' Select
     ;;
     
   *"Get Password" )
   read -p "サービス名を入力してください：" LinkService
+  gpg -d apprentice.asc > apprentice 2> /dev/null
+  
   if grep -q "$LinkService" ./apprentice; then
    ServiceName=$(grep "$LinkService" ./apprentice | cut -d , -f 1)
    UserName=$(grep "$LinkService" ./apprentice | cut -d , -f 2)
@@ -32,6 +43,8 @@ while true
 
 *"Exit")
 echo "Thank you!"
+rm apprentice
+exit
 ;;
 
 *)
